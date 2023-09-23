d8990f33be4a8830444f840931cfd5bba7cb5cb6
Yogesh Kumar <yogeshkumar@CORPLC9151525.local>
2023-09-24 01:57:32 +0530

    api-gateway is added

--- employee-service/src/main/java/com/microservices/employee/service/models/Department.java
+++ employee-service/src/main/java/com/microservices/employee/service/models/Department.java
@@ -6,10 +6,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class Department {
     private Long id;
     private String name;
--- employee-service/src/main/java/com/microservices/employee/service/models/Employee.java
+++ employee-service/src/main/java/com/microservices/employee/service/models/Employee.java
@@ -3,10 +3,12 @@ package com.microservices.employee.service.models;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
